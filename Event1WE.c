3d6c5c29f12d8df31ee19a124b5c3f7845f234f6
Nepirs <premoliclement21@gmail.com>
2023-04-17 18:19:09 +0200

    V8

---
+++ Event1WE.c
@@ -0,0 +1,19 @@
+#include<ctype.h>
+#include<stdio.h>
+#include<time.h>
+#include<stdbool.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<unistd.h>
+
+int Event1(int choix1){
+    switch
+    		while(fgets(we1, SIZE, fp3) != NULL){
+        	    int size5 = strlen(we1);
+        	    displayTxt(size5, we1);   
+	        }
+
+
+
+
+}
