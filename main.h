d41f5a0d31acedf22ded6a9e91518e7453f5fc91
lyeryne <lyeryne@gmail.com>
2023-05-15 18:26:42 +0200

    18:26_15/05_Louaye

--- main.h
+++ main.h
@@ -1,18 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "Save.h"
+#include "structures.h"
 #include "Stdtcreate.h"
 #include "DisplayTXT.h"
 #include "statPlayer.h"
 #include "Game.h"
 #include "Robust.h"
-#include "structures.h"
 
 int main();
 
